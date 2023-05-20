5a178d85e71a72a118e6f0f1e74ff04b685d3527
isaiasgj0002 <isaiasguzman863@gmail.com>
2023-05-20 15:51:50 -0500

    1.1.1

--- Agenda/Vistas/AcercaDe.xaml.cs
+++ Agenda/Vistas/AcercaDe.xaml.cs
@@ -11,6 +11,8 @@ using Agenda.Tablas;
 using System.Collections.ObjectModel;
 using System.IO;
 using Agenda.Datos;
+using Xamarin.Essentials;
+
 namespace Agenda.Vistas
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -20,11 +22,34 @@ namespace Agenda.Vistas
         {
             InitializeComponent();
             btnvolver.Clicked += Btnvolver_Clicked;
+            MessagingCenter.Subscribe<Inicio, (string, string)>(this, "ShareMessage", (sender, args) =>
+            {
+                string title = args.Item1;
+                string message = args.Item2;
+
+                // Invocar la funcionalidad de compartir del sistema operativo
+                ShareText(title, message);
+            });
         }
 
         private void Btnvolver_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new Principal());
+        }
+        private void OnShareButtonClicked(object sender, EventArgs e)
+        {
+            string message = "¡Hola! Te recomiento esta app: AgendaX. Puedes descargarla usando este enlace: https://play.google.com/store/apps/details?id=com.twoploapps.agendax";
+            string title = "Compartir";
+
+            MessagingCenter.Send(this, "ShareMessage", (title, message));
+        }
+        private async void ShareText(string title, string message)
+        {
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Title = title,
+                Text = message
+            });
         }
     }
 }
